5e0953033a90df6e3c750a32390b15ec4cc5fcc1
939653706 <40818654+939653706@users.noreply.github.com>
2023-07-18 15:52:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   appid: "wx6a2d78824accb543",
   appsecret: "7d7a126f20805c725f4374b24ea1a972",
-  template_id: "UhRrPB1qhlRyGaTbB2CpJEO6ilcZjxxq6cb1ZeTR-to",
+  template_id: "43NhIbgJ2zIkjqFP9EVRRFJMMbxa0XhyhqVLyhmiX28",
   touser: "oK1Z75rPVBMVDVas0e6DM-xCdRgc",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
